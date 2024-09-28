d12fab709db516bd88b8b1bd99fdcc9488fab9ae
gabo266 <gabo105gabriel@gmail.com>
2024-09-28 23:33:11 +0400

    new

--- level23/homework/23lesson23.py
+++ level23/homework/23lesson23.py
@@ -1,1 +1,12 @@
-#no homework to upload
+my_list = ["apple", "banana", "cherry", "date"]
+print(my_list[2]) 
+
+my_list1 = ["pinapple","peach","rib eye","filet mignon","steak"]
+print(my_list1[1:3])
+
+my_list2 = ["sausage","pear","tenderloin","pork belly","pork"]
+print(my_list2[-4:-2])
+
+my_list3 = ["meat loaf","chiken","wague","tomahawk steak","pork cheak","low tenderloin"]
+print(my_list3[-6:-3])
+print(my_list3[3:6])
---
+++ level25/homework/25lesson25.py
@@ -0,0 +1,26 @@
+print(3 + 5)
+
+print("hello " + "world")
+
+for i in range(10):
+    print(i + 1)
+
+number = 1
+while number < 11:
+    print(number)
+    number = number + 1
+
+i = 1
+while i < 51:
+    print(i)
+    i = i + 1
+
+e = 60
+while e < 101:
+    print(e)
+    e = e + 1
+
+q = 1
+while q < 51:
+    print(q)
+    q = q + q
