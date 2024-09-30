227aa987225ca38b880df00cf125155e4ba8d710
gabo266 <gabo105gabriel@gmail.com>
2024-10-01 00:34:48 +0400

    new

---
+++ level29/classwork/29lesson29.py
@@ -0,0 +1,36 @@
+list1 = ["steak","tenderloin","filet mignon","rib eye"]
+print(len(list1))
+
+list2 = ["wague","pork belly","chiken","meat"]
+list3 = ["crab","lobster","short rib"]
+list2.append(list3)
+
+list4 = ["sausege","bbq","hot dog","sushi","fried fish"]
+list4.insert(2, "burger")
+list4.insert(5, "lobster")
+
+list5 = ["fries","fish","chiken burger","fish burger","meat loaf"]
+list5.insert(3, "kfs")
+list5.insert(4, "burger king")
+
+list6 = ["pizza","taco","burito"]
+list6.pop(0,1)
+
+list7 = ["taco bell","mcdonalds","chipotle"]
+list7.pop(1,2)
+
+list8 = ["pop eye","wague a1","wague a2","wague a3","wague a4"]
+print(len(list8))
+wague = "wague a5"
+list8.append(wague)
+list8.insert(3, "wague b1")
+list8.pop(2)
+
+name1 = "petere"
+print(len(name1))
+
+name2 = "avtandili"
+print(len(name2))
+
+name3 = "nikolozi"
+print(len(name3))
---
+++ level29/home work/29lsson29.py
@@ -0,0 +1,38 @@
+list = ["meat","chiken","pork"]
+print(len(list))
+
+list1 = ["shop","crambrule","beef jerky"]
+print(len(list1))
+
+list2 = ["kfc","beef tartare"]
+print(len(list2))
+
+list3 = ["hot dog","fries","nachos","gvuakamole"]
+print(len(list3))
+
+wague1 = ("wague c2","wague c3")
+list4 = ["whine","wague c1"]
+list4.append(wague1)
+
+wague2 = ("wague c4","wague c5")
+list5 = ["shampain","walington"]
+list5.append(wague2)
+
+wague3 = ("wague b3","wague b4")
+list6 = ["wague b1","wague b2"]
+list6.append(wague3)
+
+list7 = ["wague b5","wague a1","wague a2","wague a3","wague a4"]
+list7.insert(0, "wague a5")
+list7.insert(3, "australian wague")
+
+list8 = ["japanese wague","new your strip","kobe beef","iroh","pop eye","rib eye"]
+list8.insert(2, "short rib")
+list8.insert(4, "taco")
+
+list9 = ["chipotle","burger lion","burger king","burger","mcdonalds"]
+list9.pop(3)
+list9.pop(1)
+
+words = ("hello,book,shop")
+print(len(words))
